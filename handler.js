5baaec6dc34bcfe5abfa194c0c321559fb392b72
yanaiara <yanaiara@alunos.utfpr.edu.br>
2021-07-18 12:47:02 -0300

    first route implemented

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@
 const patients = [
   {id: 1, name: 'Maria', birth: '1998-06-6'},
   {id: 2, name: 'Tania', birth: '1989-09-3'},
-  {id: 3, name: 'Josia', birth: '1967-11-7'},
+  {id: 3, name: 'Josea', birth: '1967-11-7'},
 ]
 module.exports.patientsList = async (event) => {
   return {
