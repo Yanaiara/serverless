bba5a87b138fdd7408ee681aa003b22034cd5ccc
yanaiara <yanaiara@alunos.utfpr.edu.br>
2021-07-18 11:55:17 -0300

    started serverless

---
+++ handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const patients = [
+  {id: 1, name: 'Maria', birth: '1998-06-6'},
+  {id: 2, name: 'Tania', birth: '1989-09-3'},
+  {id: 3, name: 'Josia', birth: '1967-11-7'},
+]
+module.exports.patientsList = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        patients
+      },
+      null,
+      2
+    ),
+  };
+};
